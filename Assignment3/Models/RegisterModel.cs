2fbefe216f6e26edb014a929fc71e67544272b91
CSJ <casperstenner@gmail.com>
2018-11-25 16:59:11 +0100

    Added stuff for login and registration

--- Assignment3/Models/RegisterModel.cs
+++ Assignment3/Models/RegisterModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,7 @@ namespace Assignment3.Models
         public string StaffInitials { get; set; }
         public string AUId { get; set; }
         public string Email { get; set; }
+        [DataType(DataType.Password)]
         public string Password { get; set; }
         public string PhoneNumber { get; set; }
         public Role Role { get; set; }
