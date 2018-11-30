a4d385b7e95895fbbb281316ed9739114ae22515
Aleksander Konnerup <Alek.k@hotmail.com>
2018-11-30 18:48:12 +0100

    Lad os se om det hjælper på heruko

--- Assignment3/wwwroot/js/custom.js
+++ Assignment3/wwwroot/js/custom.js
@@ -1,37 +1,5 @@
 /* HTML document is loaded. DOM is ready.
 -------------------------------------------*/
-$(function(){
-
-    ---------------------------------------------------------------------------------*/ 
-    $('.templatemo-nav').singlePageNav({
-        offset: $(".templatemo-nav").height(),
-        filter: ':not(.external)',
-        updateHash: false
-    });
-
-    /* start navigation top js */
-    $(window).scroll(function(){
-        if($(this).scrollTop()>58){
-            $(".templatemo-nav").addClass("sticky");
-        }
-        else{
-            $(".templatemo-nav").removeClass("sticky");
-        }
-    });
-    
-    /* Hide mobile menu after clicking on a link
-    -----------------------------------------------*/
-    $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-    /* end navigation top js */
-
-    $('body').bind('touchstart', function() {});
-
-    /* wow
-    -----------------*/
-    new WOW().init();
-});
 
 /* start preloader */
 $(window).load(function(){
